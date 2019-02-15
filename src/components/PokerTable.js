c6fe529799eab1c6e99ed8de708bd9230c1ac2f9
Robert Bunch <robertbunch@Roberts-MacBook-Pro.local>
2019-02-15 15:14:04 -0500

    Add buttons

--- src/components/PokerTable.js
+++ src/components/PokerTable.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"; 
 import Deck from '../utilityClasses/Deck';
+import GameButtons from './GameButtons';
 
 class PokerTable extends Component{
     constructor(){
@@ -13,10 +14,12 @@ class PokerTable extends Component{
         }
     }
 
+    
+
     render(){
         return(
             <div className="col-sm-12 the-table">
-            
+                <GameButtons />
             </div>
         )
     }
